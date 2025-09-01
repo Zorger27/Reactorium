b7f7594b1e362bff9a0cf9b0fb5b3b21b8f1ae77
Anatolii Zorin <Zorger27@GMail.com>
2025-09-01 03:55:48 +0300

    Calculator

--- src/components/other/CalculatorStandart.jsx
+++ src/components/other/CalculatorStandart.jsx
@@ -40,28 +40,28 @@ export default function CalculatorStandart() {
       </div>
 
       <div className="buttons">
-        <button onClick={handleClear}>C</button>
-        <button onClick={handleBackspace}>⌫</button>
-        <button onClick={() => handleClick("/")}>÷</button>
-        <button onClick={() => handleClick("*")}>×</button>
+        <button className="clear" onClick={handleClear}>C</button>
+        <button className="backspace" onClick={handleBackspace}>⌫</button>
+        <button className="operator" onClick={() => handleClick("/")}>÷</button>
+        <button className="operator" onClick={() => handleClick("*")}>×</button>
 
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("-")}>−</button>
+        <button className="numbers" onClick={() => handleClick("7")}>7</button>
+        <button className="numbers" onClick={() => handleClick("8")}>8</button>
+        <button className="numbers" onClick={() => handleClick("9")}>9</button>
+        <button className="operator" onClick={() => handleClick("-")}>−</button>
 
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("+")}>+</button>
+        <button className="numbers" onClick={() => handleClick("4")}>4</button>
+        <button className="numbers" onClick={() => handleClick("5")}>5</button>
+        <button className="numbers" onClick={() => handleClick("6")}>6</button>
+        <button className="operator" onClick={() => handleClick("+")}>+</button>
 
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button className="equal" onClick={handleCalculate}>=</button>
+        <button className="numbers" onClick={() => handleClick("1")}>1</button>
+        <button className="numbers" onClick={() => handleClick("2")}>2</button>
+        <button className="numbers" onClick={() => handleClick("3")}>3</button>
+        <button className="operator" className="equal" onClick={handleCalculate}>=</button>
 
-        <button className="zero" onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick(".")}>.</button>
+        <button className="numbers zero" onClick={() => handleClick("0")}>0</button>
+        <button className="dot" onClick={() => handleClick(".")}>.</button>
       </div>
     </div>
   );

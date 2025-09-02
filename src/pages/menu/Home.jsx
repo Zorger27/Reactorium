6a9a75680e7a666609e94273da4f61142bd5da79
Anatolii Zorin <Zorger27@GMail.com>
2025-09-02 03:48:36 +0300

    Small changes

--- src/pages/menu/Home.jsx
+++ src/pages/menu/Home.jsx
@@ -6,6 +6,11 @@ import {Helmet} from "@dr.pogodin/react-helmet";
 import {useSpaCleanup} from "@/hooks/useSpaCleanup.js";
 import ToggleFooterButton from "@/components/util/ToggleFooterButton.jsx";
 
+import Project1 from "@/assets/img/main/cube.webp"
+import Project2 from "@/assets/img/main/сalculator1.jpg"
+import Project3 from "@/assets/img/main/todo-list.webp"
+import Project4 from "@/assets/img/main/memory.webp"
+
 const Home = () => {
   const { t } = useTranslation();
   const siteUrl = import.meta.env.VITE_SITE_URL;
@@ -43,32 +48,52 @@ const Home = () => {
           <div className="inner">
             <Link to="/project1" className="project-card-link">
               <div className="project-card">
-                <h3>{t('project1.name')}</h3>
-                <p>{t('project1.disc')}</p>
+                <div className="project-img">
+                  <img src={ Project1 } alt="project1" />
+                </div>
+                <div className="project-text">
+                  <h3>{t('project1.name')}</h3>
+                  <p>{t('project1.disc')}</p>
+                </div>
               </div>
             </Link>
           </div>
           <div className="inner">
             <Link to="/project2" className="project-card-link">
               <div className="project-card">
-                <h3>{t('project2.name')}</h3>
-                <p>{t('project2.disc')}</p>
+                <div className="project-img">
+                  <img src={ Project2 } alt="project2" />
+                </div>
+                <div className="project-text">
+                  <h3>{t('project2.name')}</h3>
+                  <p>{t('project2.disc')}</p>
+                </div>
               </div>
             </Link>
           </div>
           <div className="inner">
             <Link to="/project3" className="project-card-link">
               <div className="project-card">
-                <h3>{t('project3.name')}</h3>
-                <p>{t('project3.disc')}</p>
+                <div className="project-img">
+                  <img src={ Project3 } alt="project3" />
+                </div>
+                <div className="project-text">
+                  <h3>{t('project3.name')}</h3>
+                  <p>{t('project3.disc')}</p>
+                </div>
               </div>
             </Link>
           </div>
           <div className="inner">
             <Link to="/project4" className="project-card-link">
               <div className="project-card">
-                <h3>{t('project4.name')}</h3>
-                <p>{t('project4.disc')}</p>
+                <div className="project-img">
+                  <img src={ Project4 } alt="project4" />
+                </div>
+                <div className="project-text">
+                  <h3>{t('project4.name')}</h3>
+                  <p>{t('project4.disc')}</p>
+                </div>
               </div>
             </Link>
           </div>

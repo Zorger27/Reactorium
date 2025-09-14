fc2f8924e82541b6c41c9d16488d63351d0c0118
Anatolii Zorin <Zorger27@GMail.com>
2025-09-14 18:30:00 +0300

    Small changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
+// import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import Sitemap from 'vite-plugin-sitemap';
+// import Sitemap from 'vite-plugin-sitemap';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const siteUrl = env.VITE_SITE_URL;
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd(), '');
+  // const siteUrl = env.VITE_SITE_URL;
 
   return {
     plugins: [
@@ -58,38 +59,38 @@ export default defineConfig(({ mode }) => {
           ]
         }
       }),
-      Sitemap({
-        hostname: siteUrl,
-        dynamicRoutes: [
-          '/about',
-          '/project1',
-          '/project2',
-          '/project3',
-          '/project4'
-        ],
-        exclude: [
-          // можно явно указать, например, "/404" или пути, которые не должны быть в sitemap
-        ],
-        readable: true,
-        changefreq: {
-          '/': 'daily',
-          '/about': 'monthly',
-          '/project1': 'weekly',
-          '/project2': 'weekly',
-          '/project3': 'weekly',
-          '/project4': 'weekly'
-        },
-        priority: {
-          '/': 1.0,
-          '/about': 0.8,
-          '/project1': 0.6,
-          '/project2': 0.6,
-          '/project3': 0.6,
-          '/project4': 0.6
-        },
-        // lastmod: new Date(), // или можно задать вручную
-        generateRobotsTxt: true
-      })
+      // Sitemap({
+      //   hostname: siteUrl,
+      //   dynamicRoutes: [
+      //     '/about',
+      //     '/project1',
+      //     '/project2',
+      //     '/project3',
+      //     '/project4'
+      //   ],
+      //   exclude: [
+      //     // можно явно указать, например, "/404" или пути, которые не должны быть в sitemap
+      //   ],
+      //   readable: true,
+      //   changefreq: {
+      //     '/': 'daily',
+      //     '/about': 'monthly',
+      //     '/project1': 'weekly',
+      //     '/project2': 'weekly',
+      //     '/project3': 'weekly',
+      //     '/project4': 'weekly'
+      //   },
+      //   priority: {
+      //     '/': 1.0,
+      //     '/about': 0.8,
+      //     '/project1': 0.6,
+      //     '/project2': 0.6,
+      //     '/project3': 0.6,
+      //     '/project4': 0.6
+      //   },
+      //   // lastmod: new Date(), // или можно задать вручную
+      //   generateRobotsTxt: true
+      // })
     ],
     resolve: {
       alias: {

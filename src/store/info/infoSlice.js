5783d4ab7bdd28f7566a737519da9863ceee9954
Anatolii Zorin <Zorger27@GMail.com>
2025-12-08 03:38:47 +0200

    Small changes

--- src/store/info/infoSlice.js
+++ src/store/info/infoSlice.js
@@ -4,31 +4,31 @@ const initialState = [
   {
     id: 1,
     title: 'React',
-    version: '19.1.0',
+    version: '19.2.1',
     url: 'https://react.dev',
   },
   {
     id: 2,
     title: 'React Router',
-    version: '7.7.1',
+    version: '7.10.1',
     url: 'https://reactrouter.com',
   },
   {
     id: 3,
     title: 'Redux Toolkit',
-    version: '2.8.2',
+    version: '2.11.0',
     url: 'https://redux-toolkit.js.org',
   },
   {
     id: 4,
     title: 'Vite',
-    version: '7.1.5',
+    version: '7.2.6',
     url: 'https://vitejs.dev',
   },
   {
     id: 5,
     title: 'React i18next',
-    version: '15.6.1',
+    version: '16.4.0',
     url: 'https://react.i18next.com',
   },
   {
@@ -46,7 +46,7 @@ const initialState = [
   {
     id: 8,
     title: "SASS",
-    version: "1.89.2",
+    version: "1.94.2",
     url: "https://sass-lang.com"
   },
   {
